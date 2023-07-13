20b6d1d38d77dfcdad930499e0024b1b3ba411d3
Samuel Lopes <samuelvitorprofissional@gmail.com>
2023-07-12 23:30:38 -0300

    u8u

--- node/terceiro.js
+++ node/terceiro.js
@@ -53,8 +53,8 @@ const server = http.createServer((req, res) => {
   
 });
 
-server.listen(3000, 'ec2-18-118-140-191.us-east-2.compute.amazonaws.com', () => {
-  console.log('Servidor em execução em http://localhost:3001/');
+server.listen(3000, 'ec2-3-22-81-96.us-east-2.compute.amazonaws.com', () => {
+  console.log('Servidor em execução em http://ec2-3-22-81-96.us-east-2.compute.amazonaws.com:3000/');
 });
 
 
